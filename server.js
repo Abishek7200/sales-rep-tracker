060355b2ff9a25d9bffbd64049b48ab8d29edcab
Abishekkumar <vabishekkumar@gmail.com>
2025-03-10 10:01:18 +0530

    updated

--- server.js
+++ server.js
@@ -59,7 +59,7 @@ const storage = multer.diskStorage({
         }
     },
     filename: (req, file, cb) => {
-        cb(null, Date.now() + "-" + file.originalname);
+        cb(null, file.originalname);
     },
 });
 
